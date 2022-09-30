d7dfb60b048442128ae0d4061d20b07e75f80234
romnnn <roman.dahm@gmx.de>
2022-09-30 21:09:16 +0200

    cleanup

--- src/types/sides.rs
+++ src/types/sides.rs
@@ -103,32 +103,6 @@ pub mod percent {
             }
         }
     }
-
-    // impl std::ops::Mul<u32> for Sides {
-    //     type Output = super::abs::Sides;
-
-    //     fn mul(self, scalar: u32) -> Self::Output {
-    //         Self::Output {
-    //             top: percent_to_abs(self.top, scalar),
-    //             left: percent_to_abs(self.left, scalar),
-    //             bottom: percent_to_abs(self.bottom, scalar),
-    //             right: percent_to_abs(self.right, scalar),
-    //         }
-    //     }
-    // }
-
-    // impl std::ops::Mul<Size> for Sides {
-    //     type Output = super::abs::Sides;
-
-    //     fn mul(self, size: Size) -> Self::Output {
-    //         Self::Output {
-    //             top: percent_to_abs(self.top, size.height),
-    //             left: percent_to_abs(self.left, size.width),
-    //             bottom: percent_to_abs(self.bottom, size.height),
-    //             right: percent_to_abs(self.right, size.width),
-    //         }
-    //     }
-    // }
 }
 
 pub mod abs {
@@ -182,48 +156,6 @@ pub mod abs {
         }
     }
 
-    // impl CheckedAdd for Sides {
-    //     type Output = Self;
-    //     type Error = ops::AddError<Self, Self>;
-
-    //     #[inline]
-    //     fn checked_add(self, rhs: Self) -> Result<Self::Output, Self::Error> {
-    //         match (|| {
-    //             let top = CheckedAdd::checked_add(self.top, rhs.top)?;
-    //             let left = CheckedAdd::checked_add(self.left, rhs.left)?;
-    //             let bottom = CheckedAdd::checked_add(self.bottom, rhs.bottom)?;
-    //             let right = CheckedAdd::checked_add(self.right, rhs.right)?;
-    //             Ok::<Self::Output, ops::AddError<u32, u32>>(Self::Output {
-    //                 top,
-    //                 left,
-    //                 bottom,
-    //                 right,
-    //             })
-    //         })() {
-    //             Ok(sides) => Ok(sides),
-    //             Err(err) => Err(ops::AddError(error::ArithmeticError {
-    //                 lhs: self,
-    //                 rhs: rhs,
-    //                 kind: None,
-    //                 cause: Some(Box::new(err)),
-    //             })),
-    //         }
-    //     }
-    // }
-
-    // impl std::ops::Add for Sides {
-    //     type Output = Self;
-
-    //     fn add(self, side: Self) -> Self::Output {
-    //         Self {
-    //             top: self.top + side.top,
-    //             right: self.right + side.right,
-    //             bottom: self.bottom + side.bottom,
-    //             left: self.left + side.left,
-    //         }
-    //     }
-    // }
-
     impl<F> CheckedMul<F> for Sides
     where
         F: numeric::NumCast + numeric::NumericType,
@@ -267,21 +199,4 @@ pub mod abs {
             }
         }
     }
-
-    // impl<F> std::ops::Mul<F> for Sides
-    // where
-    //     F: num::NumCast,
-    // {
-    //     type Output = Self;
-
-    //     fn mul(self, scalar: F) -> Self::Output {
-    //         let scalar: f32 = num::NumCast::from(scalar).unwrap();
-    //         Self {
-    //             top: (self.top as f32 * scalar).ceil() as u32,
-    //             right: (self.right as f32 * scalar).ceil() as u32,
-    //             bottom: (self.bottom as f32 * scalar).ceil() as u32,
-    //             left: (self.left as f32 * scalar).ceil() as u32,
-    //         }
-    //     }
-    // }
 }
